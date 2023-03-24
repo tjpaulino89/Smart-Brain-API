d883486de5de8436c9e82928bc82d5e7b8f5fc8a
Thomas Paulino <tjpaulino89@gmail.com>
2023-03-24 16:44:15 -0400

    bcryptjs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
