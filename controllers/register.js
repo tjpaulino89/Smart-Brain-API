b023dbf7f9d57cb1c9997ab59812d34f3f44799c
Thomas Paulino <tjpaulino89@gmail.com>
2023-03-25 22:08:19 -0400

    add catch to register

--- controllers/register.js
+++ controllers/register.js
@@ -30,7 +30,8 @@ const handleRegister = (req, res, db, bcrypt) => {
         .catch(trx.rollback)
       })
       .catch(err => res.status(400).json('Unable to register.'))
-    })
+    }).catch(err => res.status(400).json('Unable to register.'))
+
 }
 
 module.exports = {
