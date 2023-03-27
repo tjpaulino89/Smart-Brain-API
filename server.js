9fff25ffaccebe83aef5349bad3389656328d701
Thomas Paulino <tjpaulino89@gmail.com>
2023-03-27 09:38:56 -0400

    connection test

--- server.js
+++ server.js
@@ -12,13 +12,25 @@ const db = knex({
   client: 'pg',
   connection: {
     ssl: { rejectUnauthorized: false },
-    host : process.env.DATABASE_HOST,
+    host : '127.0.0.1',
     port : 5432,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PW,
-    database : process.env.DATABASE_DB
+    user : 'thomaspaulino',
+    password : '',
+    database : 'smart-brain'
   }
 });
+// host : '127.0.0.1',
+// port : 5432,
+// user : 'thomaspaulino',
+// password : '',
+// database : 'smart-brain'
+
+// ssl: { rejectUnauthorized: false },
+// host : process.env.DATABASE_HOST,
+// port : 5432,
+// user : process.env.DATABASE_USER,
+// password : process.env.DATABASE_PW,
+// database : process.env.DATABASE_DB
 
 const app = express();
 app.use(express.json());
