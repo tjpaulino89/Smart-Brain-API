d1432b5f3efee6327fc85fbcaebfe1f5a16a9304
Thomas Paulino <tjpaulino89@gmail.com>
2023-03-27 13:56:47 -0400

    added node-fetch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcryptjs');
+const fetch = "node-fetch";
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -57,7 +58,7 @@ app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res, fetch)})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
