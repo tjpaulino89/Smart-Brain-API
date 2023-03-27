265e6340138f5370131d4cbfef0a834df3e10fd5
Thomas Paulino <tjpaulino89@gmail.com>
2023-03-26 21:32:57 -0400

    update register

--- controllers/register.js
+++ controllers/register.js
@@ -30,7 +30,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         .catch(trx.rollback)
       })
       .catch(err => res.status(400).json('Unable to register.'))
-    }).catch(err => res.status(400).json('Unable to register.'))
+    })
 
 }
 
