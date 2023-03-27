9f232584749945703e7b8d1f18e0d850d80d6f99
Thomas Paulino <tjpaulino89@gmail.com>
2023-03-26 21:17:51 -0400

    update connection

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const db = knex({
   client: 'pg',
   connection: {
     ssl: { rejectUnauthorized: false },
-    host : 'dpg-cgf5fbseoogqfc3ms9s0-a.ohio-postgres.render.com',
+    host : process.env.DATABASE_HOST,
     port : 5432,
-    user : 'mydb_aan0_user',
-    password : 'JGkKzSoTyAkcB67BdK35Kjf2peaDC2oF',
-    database : 'mydb_aan0'
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB
   }
 });
 
@@ -34,8 +34,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-
 app.listen(3000, () => {console.log("App is running port 3000")})
+
 // app.listen(process.env.PORT || 3000, () => {
 //   console.log(`App is running port ${process.env.PORT}"`);
 // })
